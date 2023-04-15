0ac4b7a57523662345278dd1193fa2374443c9a0
nandobrain <prf.arturo@gmail.com>
2023-04-14 18:48:07 -0700

    friday save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import './App.css';
-import Employee from './components/employee';
+import Employee from './components/Employee';
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [role, setRole] = useState('dev')
+  const [employees, setEmployees] = useState([
+    {
+      
+      name: 'Fernando',
+      role: 'Developer',
+      img: "https://i.imgur.com/IR4SbPi.jpg",
+    },
+    {
+      name: 'Mike',
+      role: 'Developer',
+      img: "https://i.imgur.com/hpEF0KJ.jpg",
+    },
+    {
+      name: 'Victoria',
+      role: 'doc',
+      img: "https://i.imgur.com/Yw7BKMX.jpg",
+    },
+    {
+      name: 'Taylor',
+      role: 'owner',
+      img: "https://i.imgur.com/okVG62B.jpg",
+    },
+    {
+      name: 'Rex',
+      role: 'security',
+      img: "https://i.imgur.com/IR4SbPi.jpg",
+    },
+    {
+      name: 'Jesse',
+      role: 'Developer',
+      img: "https://i.imgur.com/ojtreYh.jpg",
+    },
+  ])
   
   const showEmployees = true;
     return (
@@ -17,9 +52,21 @@ function App() {
           console.log(e.target.value);
           setRole(e.target.value)
         }}/>
-          <Employee name="fernando" role="intern"/>
-          <Employee name="aby" />
-          <Employee name="mike" />
+
+        <div className='flex flex-wrap justify-center'>
+            {employees.map((employee) => {
+              
+              return (
+                <Employee 
+                  key={uuidv4()} 
+                  name={employee.name} 
+                  role={employee.role} 
+                  img={employee.img}
+                />
+              );  
+            })}
+        
+        </div>
         </>
           )  : (
           <p>You cannot see the employees</p>
